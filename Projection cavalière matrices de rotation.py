c7910ca16e11c6ba3e10239f009206517027e459
Thermo <78959190+Fusion-thermo@users.noreply.github.com>
2022-09-10 11:40:33 +0200

    Add files via upload
    Màj

--- Projection cavalière matrices de rotation.py
+++ Projection cavalière matrices de rotation.py
@@ -1,26 +1,22 @@
 from tkinter import *
 from math import sqrt, acos, atan2, cos, sin, pi
+import numpy as np
 
 hauteur=800
 largeur=1100
 origine_x=largeur//2
 origine_y=hauteur//2
-unite=50
+unite=100
 rayon_point=1
 coeff_distance=2
 
 '''
 coordonnées cartésiennes pour l'affichage sur l'écran 2D cartésien et pour les 2 translations
-coordonnées sphériques pour le zoom (égal à une translation selon x si on était en perspective mais ce n'est pas le cas ici)
-coos cylindriques ou sphériques pour les 2 rotations, peu importe lesquelles
+matrices de rotation pour les rotations
 il manque une rotation autour de x mais elle n'a pas d'intérêt, et une translation selon x sans intérêt
 
-phi1 correspond au curseur phi et phi2 correspond au curseur theta
 '''
 
-		#self.r=sqrt(x**2 + z**2)
-		#self.theta_c=atan2(-z,x)
-		#self.z_c=y
 
 
 def radians(angle):
@@ -36,30 +32,12 @@ class point:
 		self.x=x
 		self.y=y
 		self.z=z
-		#coos sphériques 1
-		#avec ces notations : https://fr.wikipedia.org/wiki/Coordonn%C3%A9es_sph%C3%A9riques
-		self.rho=sqrt(x**2 + y**2 + z**2)
-		if self.rho!=0:
-			self.theta1=acos(z/self.rho)
-		else:
-			self.theta1=0
+		#Rz
 		self.phi1=atan2(y,x)
-		self.rho_initial=self.rho
-		self.theta_initial1=self.theta1
 		self.phi_initial1=self.phi1
-		self.theta_degre1=int(degre(self.theta1))
-		self.phi_degre1=int(degre(self.phi1))
-		#coos sphériques 2
-		#une sphère tournée de 90° autour de x : le z va vers le -y 
-		if self.rho!=0:
-			self.theta2=acos(-y/self.rho)
-		else:
-			self.theta2=0
-		self.phi2=atan2(z,x)
-		self.theta_initial2=self.theta2
+		#Ry
+		self.phi2=atan2(x,z)
 		self.phi_initial2=self.phi2
-		self.theta_degre2=int(degre(self.theta2))
-		self.phi_degre2=int(degre(self.phi2))
 		#décalages précédents
 		self.decalage_y_prec=0
 		self.decalage_z_prec=0
@@ -73,58 +51,52 @@ class point:
 			self.nom=str((round(x,1),round(y,1),round(z,1)))
 		else:
 			self.nom=nom
-	def zoom(self,rho):
-		if self.rho_initial!=0:
-			self.rho=self.rho_initial+rho
-			self.x=self.rho*sin(self.theta1)*cos(self.phi1)
-			self.y=self.rho*sin(self.theta1)*sin(self.phi1)
-			self.z=self.rho*cos(self.theta1)
-			if "(" in self.nom:
-				self.nom=str((round(self.x,1),round(self.y,1),round(self.z,1)))
 
-	def rotate_sphere1(self,phi):
+	def rotate_Rz(self,phi1):
 		'''
 		seul phi1 bouge
 		on recalcule x, y z
-		il faut donc recalculer theta2 et phi2
-		problème : le décalage indiqué sur l'écran avec les curseurs ne correspond plus aud écalage réel.
+		problème : le décalage indiqué sur l'écran avec les curseurs ne correspond plus au décalage réel.
 		pour corriger ça et donc avoir un décalage cohérent pour tous les points, c'est la valeur d'origine dans "décalage par rapport à la valeur d'origine" qu'il faut changer
 		donc je recalcule la valeur d'origine
 		'''
-		self.phi1=self.phi_initial1+phi
-		self.x=self.rho*sin(self.theta1)*cos(self.phi1)
-		self.y=self.rho*sin(self.theta1)*sin(self.phi1)
-		self.z=self.rho*cos(self.theta1)
+		theta=(phi1+self.phi_initial1)-self.phi1
+		self.phi1=self.phi_initial1+phi1
+		
+		coos=np.array([[self.x],[self.y],[self.z]])
+		Rz=np.array([[cos(theta),-sin(theta),0],[sin(theta),cos(theta),0],[0,0,1]])
+		result=np.dot(Rz,coos)
+		#print(coos,result)
+		self.x=result[0][0]
+		self.y=result[1][0]
+		self.z=result[2][0]
 
-		if self.rho!=0:
-			self.theta2=acos(-self.y/self.rho)
-		else:
-			self.theta2=0
-		self.phi2=atan2(self.z,self.x)
-		self.phi_initial2=self.phi2 - radians(int(theta.get()))
+		self.phi2=atan2(self.x,self.z)
+		self.phi_initial2=self.phi2 - radians(int(Ry.get()))
 
 		if "(" in self.nom:
 			self.nom=str((round(self.x,1),round(self.y,1),round(self.z,1)))
-	
-	def rotate_sphere2(self,phi2):
+
+	def rotate_Ry(self,phi2):
 		'''
-		sphère décalée de 90° par rapport à la première
-		sinon même principe
+		même principe
 		'''
+		theta=(phi2+self.phi_initial2)-self.phi2
 		self.phi2=self.phi_initial2+phi2
-		self.x=self.rho*sin(self.theta2)*cos(self.phi2)
-		self.z=self.rho*sin(self.theta2)*sin(self.phi2)
-		self.y=self.rho*-1*cos(self.theta2)
 
-		if self.rho!=0:
-			self.theta1=acos(self.z/self.rho)
-		else:
-			self.theta1=0
+		coos=np.array([[self.x],[self.y],[self.z]])
+		Ry=np.array([[cos(theta),0,sin(theta)],[0,1,0],[-sin(theta),0,cos(theta)]])
+		result=np.dot(Ry,coos)
+		self.x=result[0][0]
+		self.y=result[1][0]
+		self.z=result[2][0]
+
 		self.phi1=atan2(self.y,self.x)
-		self.phi_initial1=self.phi1 - radians(int(phi.get()))
+		self.phi_initial1=self.phi1 - radians(int(Rz.get()))
 
 		if "(" in self.nom:
 			self.nom=str((round(self.x,1),round(self.y,1),round(self.z,1)))
+
 	def deplace_y(self,y):
 		if y-self.decalage_y_prec>0:
 			self.y-=0.5
@@ -132,20 +104,11 @@ class point:
 			self.y+=0.5
 		self.decalage_y_prec=y
 
-		self.rho=sqrt(self.x**2 + self.y**2 + self.z**2)
-		self.rho_initial=self.rho-float(rho.get())
-		if self.rho!=0:
-			self.theta2=acos(-self.y/self.rho)
-		else:
-			self.theta2=0
-		self.phi2=atan2(self.z,self.x)
-		self.phi_initial2=self.phi2 - radians(int(theta.get()))
-		if self.rho!=0:
-			self.theta1=acos(self.z/self.rho)
-		else:
-			self.theta1=0
+		self.phi2=atan2(self.x,self.z)
+		self.phi_initial2=self.phi2 - radians(int(Ry.get()))
+	
 		self.phi1=atan2(self.y,self.x)
-		self.phi_initial1=self.phi1 - radians(int(phi.get()))
+		self.phi_initial1=self.phi1 - radians(int(Rz.get()))
 
 	def deplace_z(self,z):
 		if z-self.decalage_z_prec>0:
@@ -154,50 +117,35 @@ class point:
 			self.z+=0.5
 		self.decalage_z_prec=z
 
-		self.rho=sqrt(self.x**2 + self.y**2 + self.z**2)
-		self.rho_initial=self.rho-float(rho.get())
-		if self.rho!=0:
-			self.theta2=acos(-self.y/self.rho)
-		else:
-			self.theta2=0
-		self.phi2=atan2(self.z,self.x)
-		self.phi_initial2=self.phi2 - radians(int(theta.get()))
-		if self.rho!=0:
-			self.theta1=acos(self.z/self.rho)
-		else:
-			self.theta1=0
+		self.phi2=atan2(self.x,self.z)
+		self.phi_initial2=self.phi2 - radians(int(Ry.get()))
+		
 		self.phi1=atan2(self.y,self.x)
-		self.phi_initial1=self.phi1 - radians(int(phi.get()))
+		self.phi_initial1=self.phi1 - radians(int(Rz.get()))
 
 
 
 longueur_repere=5
-u=5
+u=3
 repere=[point(0,0,0,"0"),point(0,longueur_repere,0,"y"),point(longueur_repere,0,0,"x"),point(0,0,longueur_repere,"z")]
 carre=[point(-u,-u,-u),point(-u,u,-u),point(u,u,-u),point(u,-u,-u),point(-u,-u,u),point(-u,u,u),point(u,u,u),point(u,-u,u)]
 #carre=[point(1,1,1)]
 distance_plan=20
 camera=point(50,0,0)
 
-def rotation_sphere1(rien,points=carre,points_repere=repere):
+def rotation_Ry(rien,points=carre,points_repere=repere):
 	for dot in points:
-		dot.rotate_sphere1(radians(int(phi.get())))
+		dot.rotate_Ry(radians(int(Ry.get())))
 	for dot in points_repere:
-		dot.rotate_sphere1(radians(int(phi.get())))
+		dot.rotate_Ry(radians(int(Ry.get())))
 	affichage_carre(points)
 	affichage_repere(points_repere)
 
-def rotation_sphere2(rien,points=carre,points_repere=repere):
+def rotation_Rz(rien,points=carre,points_repere=repere):
 	for dot in points:
-		dot.rotate_sphere2(radians(int(theta.get())))
+		dot.rotate_Rz(radians(int(Rz.get())))
 	for dot in points_repere:
-		dot.rotate_sphere2(radians(int(theta.get())))
-	affichage_carre(points)
-	affichage_repere(points_repere)
-
-def zoom(rien,points=carre,points_repere=repere):
-	for dot in points:
-		dot.zoom(float(rho.get()))
+		dot.rotate_Rz(radians(int(Rz.get())))
 	affichage_carre(points)
 	affichage_repere(points_repere)
 
@@ -217,7 +165,6 @@ def deplace_z(rien,points=carre,points_repere=repere):
 def affichage_carre(points):
 	#debug(points)
 	Canevas.delete(ALL)
-	#si on dit que le x des coos 3D est ramené à 0 alors tous les points sont sur un plan avec y et z comme coos en 2D : ombre du 3D sur un plan forme du 2D
 	l=[]
 	for dot1 in points:
 		for dot2 in points:
@@ -226,8 +173,8 @@ def affichage_carre(points):
 	while 0 in l:
 		l.remove(0)
 	mini=l[0]*1.05
-
 	#Perspective : fais comme si un caméra regarde un objet 3D et qu'il se projette sur un plan entre les deux, selon les lignes qui vont de la camera à chaque point.
+
 	for dot1 in points:
 		coeff1=(distance_plan - dot1.x)/(camera.x - dot1.x)
 		dot1.py=coeff1 * (camera.y-dot1.y)
@@ -272,20 +219,15 @@ Canevas.pack(side=LEFT)
 
 
 
-theta=StringVar()
-theta.set(0)
-angle_theta=Scale(fenetre,  orient='vertical',  from_=360,  to=0,  resolution=1,  tickinterval=120,  label='Theta',  variable=theta,  command=rotation_sphere2)
-angle_theta.pack()
+Ry=StringVar()
+Ry.set(0)
+angle_Ry=Scale(fenetre,  orient='vertical',  from_=360,  to=0,  resolution=1,  tickinterval=120,  label='Ry',  variable=Ry,  command=rotation_Ry)
+angle_Ry.pack()
 
-phi=StringVar()
-phi.set(0)
-angle_phi=Scale(fenetre,  orient='horizontal',  from_=0,  to=360,  resolution=1,  tickinterval=120,  label='Phi',  variable=phi,  command=rotation_sphere1)
-angle_phi.pack()
-
-rho=StringVar()
-rho.set(0)
-longueur_rho=Scale(fenetre,  orient='horizontal',  from_=0,  to=3,  resolution=0.1,  tickinterval=1,  label='Rho',  variable=rho,  command=zoom)
-longueur_rho.pack()
+Rz=StringVar()
+Rz.set(0)
+angle_Rz=Scale(fenetre,  orient='horizontal',  from_=0,  to=360,  resolution=1,  tickinterval=120,  label='Rz',  variable=Rz,  command=rotation_Rz)
+angle_Rz.pack()
 
 deplacement_y=StringVar()
 deplacement_y.set(0)
