ba4306361f2b530f47fb34ce4365a37debb6c455
Jean BERTHAUD <jean.berthaud@outlook.com>
2023-04-14 15:39:58 +0200

    Voir description
    Meilleur calcul de l'épaisseur des traits et la taille de rayon.
    Ajout des rotations automatiques
    Meilleures visualisations de 120 et 600

--- 4-polytope régulier convexe tests.py
+++ 4-polytope régulier convexe tests.py
@@ -4,6 +4,7 @@ import numpy as np
 import itertools
 from sympy.combinatorics import Permutation
 import scipy.constants
+from time import sleep
 
 g_ratio=scipy.constants.golden
 
@@ -11,8 +12,6 @@ hauteur=900
 largeur=1150
 origine_x=largeur//2
 origine_y=hauteur//2
-rayon_point=4
-coeff_distance=2
 
 '''
 coordonnées cartésiennes pour l'affichage sur l'écran 2D cartésien et pour les 2 translations
@@ -23,7 +22,7 @@ une lumière 4D éclaire l'objet le projetant en 3D. Puis on projette ces coos 3
 
 '''
 
-def radians(angle):
+def radian(angle):
 	return angle*pi/180
 
 def permutations(coos):
@@ -61,13 +60,7 @@ class point:
 		self.Rzw=0
 		self.Rzw_initial=0
 		#point projeté en 3D
-		self.x3d=0
-		self.y3d=0
-		self.z3d=0
-		#point projeté en 2D
-		self.y2d=0
-		self.z2d=0
-
+		self.projections()
 
 		if nom=="":
 			self.nom=str((round(x,3),round(y,3),round(z,3)))
@@ -142,10 +135,10 @@ class point:
 
 		if "(" in self.nom:
 			self.nom=str((round(self.x,3),round(self.y,3),round(self.z,3)))
-	def projections(self,objet):
+	def projections(self):
 		#3D
 		coos=np.array([[self.x],[self.y],[self.z],[self.w]])
-		coeff=1/(lumiere.w - self.w)
+		coeff=1/(float(lumiere_w_var.get()) - self.w)
 		R=np.array([[coeff,0,0,0],[0,coeff,0,0],[0,0,coeff,0]])
 		result=np.dot(R,coos)
 		self.x3d=result[0][0]
@@ -154,68 +147,85 @@ class point:
 
 		#2D
 		#Perspective : fais comme si un caméra regarde un objet 3D et qu'il se projette sur un plan entre les deux, selon les lignes qui vont de la camera à chaque point.
-		coeff1=(objet.distance_plan - self.x3d)/(objet.camera.x - self.x3d)
-		self.y2d=coeff1 * (objet.camera.y-self.y3d)
-		self.z2d=coeff1 * (objet.camera.z-self.z3d)
+		coeff1=(int(distance_plan_var.get()) - self.x3d)/(int(camera_x_var.get()) - self.x3d)
+		self.y2d=coeff1 * (-self.y3d)#(objet.camera.y-self.y3d) --> y=0
+		self.z2d=coeff1 * (-self.z3d)#(objet.camera.z-self.z3d) --> z=0
 
-lumiere=point(0,0,0,1.5)
+#lumiere=point(0,0,0,1.5)
 
 class objet_4D:
-	def __init__(self,points,longueur_arrete,unite,distance_plan,camera):
+	def __init__(self,points,longueur_arrete):
 		self.points=points
-		self.unite=unite
 		self.longueur_arrete=longueur_arrete
-		self.distance_plan=distance_plan
-		self.camera=camera
 		self.arretes=[]
 		for dot1 in self.points:
 			for dot2 in self.points:
 				length=sqrt((dot1.x-dot2.x)**2 + (dot1.y-dot2.y)**2 + (dot1.z-dot2.z)**2 + (dot1.w-dot2.w)**2)
 				if round(length,2)==round(self.longueur_arrete,2) and (dot1,dot2) not in self.arretes and (dot2,dot1) not in self.arretes:
 					self.arretes.append((dot1,dot2))
-					print(round(length,2),round(self.longueur_arrete,2),"T")
-				elif round(length,2)!=round(self.longueur_arrete,2):
-					print(round(length,2),round(self.longueur_arrete,2),False)
+				# 	print(round(length,2),round(self.longueur_arrete,2),"T")
+				# elif round(length,2)!=round(self.longueur_arrete,2):
+				# 	print(round(length,2),round(self.longueur_arrete,2),False)
+		#on cherche le point moyen le plus proche et celui le plus éloigné pour l'affichage de la largeur des lignes
+		moyenne_largeur=[(dots[0].x3d+dots[1].x3d)/2 for dots in self.arretes]
+		self.min_moyenne_largeur=min(moyenne_largeur)
+		self.max_moyenne_largeur=max(moyenne_largeur)
+		#on cherche le point le plus éloigné et le point le plus proche pour trouver le rayon des points
+		position_points=[dots[0].x3d for dots in self.arretes] + [dots[1].x3d for dots in self.arretes]
+		self.min_distance_point=min(position_points)
+		self.max_distance_point=max(position_points)
+		#print(self.min_moyenne_largeur,self.max_moyenne_largeur, moyenne_largeur)
 				
 		print("{} arrêtes".format(len(self.arretes)))
 
 def rotation_Rxz(rien):
 	global objet
 	for dot in objet.points:
-		dot.rotate_Rxz(radians(int(Rxz.get())))
+		dot.rotate_Rxz(radian(int(Rxz.get())))
 	affichage_objet(objet)
 
 def doublerotate(rien):
 	global objet
 	for dot in objet.points:
-		dot.doublerotate(radians(int(Rw.get())))
+		dot.doublerotate(radian(int(Rw.get())))
 	affichage_objet(objet)
 
 def rotation_Rxy(rien):
 	global objet
 	for dot in objet.points:
-		dot.rotate_Rxy(radians(int(Rxy.get())))
+		dot.rotate_Rxy(radian(int(Rxy.get())))
 	affichage_objet(objet)
 
 def rotation_Rzw(rien):
 	global objet
 	for dot in objet.points:
-		dot.rotate_Rzw(radians(int(Rzw.get())))
+		dot.rotate_Rzw(radian(int(Rzw.get())))
 	affichage_objet(objet)
 
 
 def affichage_objet(objet):
 	Canevas.delete(ALL)
+	unite=int(unite_var.get())
 	for dot in objet.points:
-		dot.projections(objet)
-		rayon=rayon_point*(objet.distance_plan-dot.x3d*coeff_distance)*(objet.distance_plan/objet.camera.x)
-		Canevas.create_oval(origine_x + dot.y2d * objet.unite - rayon, origine_y - dot.z2d * objet.unite - rayon, origine_x + dot.y2d * objet.unite + rayon, origine_y - dot.z2d * objet.unite + rayon,outline="red",width=2)
-		#create_text pour avoir les coos à côté des points et create_oval pour avoir la distance des points par rapport au plan visualisée (pour savoir s'il est avant ou après l'origine)
-		#Canevas.create_text(origine_x + dot.y2d* objet.unite,origine_y - dot.z2d* objet.unite,text=dot.nom)
+		dot.projections()
+		rayon = 10 - 9 * (dot.x3d - objet.min_distance_point)/(objet.max_distance_point - objet.min_distance_point)
+		if rayon<0:
+			rayon=0
+		elif rayon>10:
+			rayon=10
+		Canevas.create_oval(origine_x + dot.y2d * unite - rayon, origine_y - dot.z2d * unite - rayon, origine_x + dot.y2d * unite + rayon, origine_y - dot.z2d * unite + rayon,outline="red",width=2)
+		#create_text pour avoir les coos à côté des points et create_oval pour avoir la distance des points par rapport au plan visualisé
+		#Canevas.create_text(origine_x + dot.y2d* unite,origine_y - dot.z2d* unite,text=dot.nom)
 	for dots in objet.arretes:
 		moyenne=(dots[0].x3d+dots[1].x3d)/2
-		Canevas.create_line(origine_x + dots[0].y2d* objet.unite,origine_y - dots[0].z2d* objet.unite,origine_x + dots[1].y2d* objet.unite,origine_y - dots[1].z2d* objet.unite,fill="blue",width=(objet.distance_plan-moyenne)*(objet.distance_plan/objet.camera.x))
-
+		#largeur comprise entre 1 et 2
+		largeur_ligne=2 - (moyenne - objet.min_moyenne_largeur)/(objet.max_moyenne_largeur - objet.min_moyenne_largeur)
+		if largeur_ligne<1:
+			largeur_ligne=1
+		elif largeur_ligne>2:
+			largeur_ligne=2
+		#print(origine_x + dots[0].y2d* unite,origine_y - dots[0].z2d* unite,origine_x + dots[1].y2d* unite,origine_y - dots[1].z2d* unite,largeur_ligne)
+		Canevas.create_line(origine_x + dots[0].y2d* unite,origine_y - dots[0].z2d* unite,origine_x + dots[1].y2d* unite,origine_y - dots[1].z2d* unite,fill="blue",width = largeur_ligne)
 
 def initialisation():
 	#all 6 convex regular 4-polytope = 4-polytope régulier convexe
@@ -230,38 +240,54 @@ def initialisation():
 		#5-cell
 		coos=[point(1,1,1,-1/sqrt(5)),point(1,-1,-1,-1/sqrt(5)),point(-1,1,-1,-1/sqrt(5)),point(-1,-1,1,-1/sqrt(5)),point(0,0,0,4/sqrt(5))]
 		longueur_arrete=2*sqrt(2)
-		objet=objet_4D(coos,longueur_arrete,400,3,point(5,0,0))
+		objet=objet_4D(coos,longueur_arrete)
+		unite_var.set(400)
+		distance_plan_var.set(3)
+		camera_x_var.set(5)
+		lumiere_w_var.set(1.5)
 	elif choix=="2":
 		#8-cell
 		coos_tesseract=permutations((0.5,0.5,0.5,0.5))[:]+permutations((-0.5,0.5,0.5,0.5))[:]+permutations((-0.5,-0.5,0.5,0.5))[:]+permutations((-0.5,-0.5,-0.5,0.5))[:]+permutations((-0.5,-0.5,-0.5,-0.5))[:]
 		coos=[point(i[0],i[1],i[2],i[3]) for i in coos_tesseract]
 		longueur_arrete=1
-		objet=objet_4D(coos,longueur_arrete,880,3,point(5,0,0))
+		objet=objet_4D(coos,longueur_arrete)
+		unite_var.set(880)
+		distance_plan_var.set(3)
+		camera_x_var.set(5)
+		lumiere_w_var.set(1.5)
 	elif choix=="3":
 		#16 cell
 		coos_hexadecachore=permutations((1,0,0,0))[:]+permutations((-1,0,0,0))[:]
 		coos=[point(i[0],i[1],i[2],i[3]) for i in coos_hexadecachore]
 		longueur_arrete=sqrt(2)
-		objet=objet_4D(coos,longueur_arrete,700,3,point(5,0,0))
+		objet=objet_4D(coos,longueur_arrete)
+		unite_var.set(700)
+		distance_plan_var.set(3)
+		camera_x_var.set(5)
+		lumiere_w_var.set(1.5)
 	elif choix=="4":
 		#24-cell
 		coos_icositetrachore=permutations((1,1,0,0))[:]+permutations((1,-1,0,0))[:]+permutations((-1,-1,0,0))[:]
 		coos=[point(i[0],i[1],i[2],i[3]) for i in coos_icositetrachore]
 		longueur_arrete=sqrt(2)
-		objet=objet_4D(coos,longueur_arrete,300,3,point(5,0,0))
+		objet=objet_4D(coos,longueur_arrete)
+		unite_var.set(300)
+		distance_plan_var.set(3)
+		camera_x_var.set(5)
+		lumiere_w_var.set(1.5)
 	elif choix=="5":
 		#120-cell
 		coos_hecatonicosachore=permutations((0,0,2,2))[:]+permutations((0,0,-2,2))[:]+permutations((0,0,-2,-2))[:]
-		a=len(coos_hecatonicosachore)
+		#a=len(coos_hecatonicosachore)
 		#print(a)
 		coos_hecatonicosachore+=permutations((1,1,1,sqrt(5)))[:]+permutations((-1,1,1,sqrt(5)))[:]+permutations((-1,-1,1,sqrt(5)))[:]+permutations((-1,-1,-1,sqrt(5)))[:] + permutations((1,1,1,-sqrt(5)))[:]+permutations((-1,1,1,-sqrt(5)))[:]+permutations((-1,-1,1,-sqrt(5)))[:]+permutations((-1,-1,-1,-sqrt(5)))[:]
-		b=len(coos_hecatonicosachore)
+		#b=len(coos_hecatonicosachore)
 		#print(b-a)
 		coos_hecatonicosachore+=permutations((g_ratio,g_ratio,g_ratio,1/g_ratio**2))[:]+permutations((-g_ratio,g_ratio,g_ratio,1/g_ratio**2))[:]+permutations((-g_ratio,-g_ratio,g_ratio,1/g_ratio**2))[:]+permutations((-g_ratio,-g_ratio,-g_ratio,1/g_ratio**2))[:] + permutations((g_ratio,g_ratio,g_ratio,-1/g_ratio**2))[:]+permutations((-g_ratio,g_ratio,g_ratio,-1/g_ratio**2))[:]+permutations((-g_ratio,-g_ratio,g_ratio,-1/g_ratio**2))[:]+permutations((-g_ratio,-g_ratio,-g_ratio,-1/g_ratio**2))[:]
-		c=len(coos_hecatonicosachore)
+		#c=len(coos_hecatonicosachore)
 		#print(c-b)
 		coos_hecatonicosachore+=permutations((1/g_ratio,1/g_ratio,1/g_ratio,g_ratio**2))[:]+permutations((-1/g_ratio,1/g_ratio,1/g_ratio,g_ratio**2))[:]+permutations((-1/g_ratio,-1/g_ratio,1/g_ratio,g_ratio**2))[:]+permutations((-1/g_ratio,-1/g_ratio,-1/g_ratio,g_ratio**2))[:] + permutations((1/g_ratio,1/g_ratio,1/g_ratio,-g_ratio**2))[:]+permutations((-1/g_ratio,1/g_ratio,1/g_ratio,-g_ratio**2))[:]+permutations((-1/g_ratio,-1/g_ratio,1/g_ratio,-g_ratio**2))[:]+permutations((-1/g_ratio,-1/g_ratio,-1/g_ratio,-g_ratio**2))[:]
-		d=len(coos_hecatonicosachore)
+		#d=len(coos_hecatonicosachore)
 		#print(d-c)
 		#print(a,b,c,d)
 
@@ -292,14 +318,18 @@ def initialisation():
 		#print(len(coos))
 		longueur_arrete=2/g_ratio**2
 		#print(longueur_arrete)
-		objet=objet_4D(coos,longueur_arrete,700,30,point(1000,0,0))
-	else:
+		objet=objet_4D(coos,longueur_arrete)
+		unite_var.set(1000)
+		distance_plan_var.set(400)
+		camera_x_var.set(2700)
+		lumiere_w_var.set(3.0)#bug avec cette valeur : si ce n'est pas 1.5 et qu'on passe du 120 au 600, les valeurs min et max des points sont incorrectes, ce qui perturbe la largeur des lignes et le rayon des cercles. 
+	elif choix=="6":
 		#600-cell
 		coos_hexacosichore=permutations((2,0,0,0))[:]+permutations((-2,0,0,0))[:]
-		a=len(coos_hexacosichore)
+		#a=len(coos_hexacosichore)
 		#print(a)
 		coos_hexacosichore+=permutations((1,1,1,1))[:]+permutations((-1,1,1,1))[:]+permutations((-1,-1,1,1))[:]+permutations((-1,-1,-1,1))[:]+permutations((-1,-1,-1,-1))[:]
-		b=len(coos_hexacosichore)
+		#b=len(coos_hexacosichore)
 		#print(b-a)
 		permut=permutations((0,1,2,3))[:]
 		parite_paire=[]
@@ -318,10 +348,80 @@ def initialisation():
 		coos=[point(i[0],i[1],i[2],i[3]) for i in coos_hexacosichore]
 		#print(len(coos))
 		longueur_arrete=2/g_ratio
-		objet=objet_4D(coos,longueur_arrete,700,3,point(5,0,0))
+		objet=objet_4D(coos,longueur_arrete)
+		unite_var.set(210)
+		distance_plan_var.set(100)
+		camera_x_var.set(500)
+		lumiere_w_var.set(1.5)
 
+	#preuve du bug :
+	# print(objet.max_distance_point, objet.min_distance_point,objet.min_moyenne_largeur,objet.max_moyenne_largeur)
 	affichage_objet(objet)
 
+revolution=0
+revolution_angle_init=0
+def revolution_xz():
+	recursif = fenetre.after(10,revolution_xz)
+	global objet, revolution, revolution_angle_init
+	if revolution==0:
+		revolution+=1
+		revolution_angle_init=int(Rxz.get())
+	elif revolution<360:
+		revolution+=1
+		teta=(revolution_angle_init+revolution)%360
+		Rxz.set(teta)
+		rotation_Rxz("")
+	else:
+		revolution=0
+		fenetre.after_cancel(recursif)
+
+def revolution_xy():
+	recursif = fenetre.after(10,revolution_xy)
+	global objet, revolution, revolution_angle_init
+	if revolution==0:
+		revolution+=1
+		revolution_angle_init=int(Rxy.get())
+	elif revolution<360:
+		revolution+=1
+		teta=(revolution_angle_init+revolution)%360
+		Rxy.set(teta)
+		rotation_Rxy("")
+	else:
+		revolution=0
+		fenetre.after_cancel(recursif)
+
+def revolution_zw():
+	recursif = fenetre.after(10,revolution_zw)
+	global objet, revolution, revolution_angle_init
+	if revolution==0:
+		revolution+=1
+		revolution_angle_init=int(Rzw.get())
+	elif revolution<360:
+		revolution+=1
+		teta=(revolution_angle_init+revolution)%360
+		Rzw.set(teta)
+		rotation_Rzw("")
+	else:
+		revolution=0
+		fenetre.after_cancel(recursif)
+
+def revolution_w():
+	recursif = fenetre.after(10,revolution_w)
+	global objet, revolution, revolution_angle_init
+	if revolution==0:
+		revolution+=1
+		revolution_angle_init=int(Rw.get())
+	elif revolution<360:
+		revolution+=1
+		teta=(revolution_angle_init+revolution)%360
+		Rw.set(teta)
+		doublerotate("")
+	else:
+		revolution=0
+		fenetre.after_cancel(recursif)
+
+def tester_parametres(rien):
+	affichage_objet(objet)
 
 fenetre=Tk()
 fenetre.attributes('-fullscreen', True)
@@ -331,7 +431,7 @@ Canevas.pack(side=LEFT)
 
 Rxz=StringVar()
 Rxz.set(0)
-angle_Rxz=Scale(fenetre,  orient='vertical',  from_=360,  to=0,  resolution=1,  tickinterval=120,  label='Rotation XZ',  variable=Rxz,  command=rotation_Rxz)
+angle_Rxz=Scale(fenetre,  orient='horizontal',  from_=0,  to=360,  resolution=1,  tickinterval=120,  label='Rotation XZ',  variable=Rxz,  command=rotation_Rxz)
 angle_Rxz.pack()
 
 Rxy=StringVar()
@@ -352,6 +452,9 @@ angle_Rw.pack()
 demarrer = Button(fenetre,  text = 'Start',  command = initialisation)
 demarrer.pack()
 
+Bouton1 = Button(fenetre,  text = 'Quitter - Close',  command = fenetre.destroy)
+Bouton1.pack()
+
 value=StringVar()
 value.set(2)
 Choix1=Radiobutton(fenetre, text="Pentachore - 5 cell",variable=value, value=1)
@@ -367,7 +470,38 @@ Choix4.pack()
 Choix5.pack()
 Choix6.pack()
 
-Bouton1 = Button(fenetre,  text = 'Quitter - Close',  command = fenetre.destroy)
-Bouton1.pack()
+Revolution_xz = Button(fenetre,  text = 'Revolution xz',  command = revolution_xz)
+Revolution_xz.pack()
+Revolution_xy = Button(fenetre,  text = 'Revolution xy',  command = revolution_xy)
+Revolution_xy.pack()
+Revolution_zw = Button(fenetre,  text = 'Revolution zw',  command = revolution_zw)
+Revolution_zw.pack()
+Revolution_w = Button(fenetre,  text = 'Revolution w',  command = revolution_w)
+Revolution_w.pack()
+
+
+
+#Sliders pour trouver la meilleure visualisation de chaque objet
+unite_var=StringVar()
+unite_var.set(0)
+#unite_scale=Scale(fenetre,  orient='horizontal',  from_=1,  to=1000,  resolution=1,  tickinterval=300,  label='Unité = zoom',  variable=unite_var, command=tester_parametres)
+#unite_scale.pack()
+
+distance_plan_var=StringVar()
+distance_plan_var.set(0)
+#distance_plan_scale=Scale(fenetre,  orient='horizontal',  from_=0,  to=500,  resolution=1,  tickinterval=250,  label='Distance du plan de projection',  variable=distance_plan_var, command=tester_parametres)
+#distance_plan_scale.pack()
+
+camera_x_var=StringVar()
+camera_x_var.set(0)
+#camera_x_scale=Scale(fenetre,  orient='horizontal',  from_=1,  to=3001,  resolution=1,  tickinterval=1500,  label='Distance de la caméra',  variable=camera_x_var, command=tester_parametres)
+#camera_x_scale.pack()
+
+lumiere_w_var=StringVar()
+lumiere_w_var.set(0)
+#lumiere_w_scale=Scale(fenetre,  orient='horizontal',  from_=0.1,  to=5,  resolution=0.1,  tickinterval=1,  label='Lumière',  variable=lumiere_w_var, command=tester_parametres)
+#lumiere_w_scale.pack()
+
+
 
 fenetre.mainloop()
